f79f2fa28554a09a5c00ea4a8e3ccecb340304bb
shivgirja <amitsrivastava115@yahoo.com>
2021-11-21 15:07:43 +0530

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Amit Shrivastava on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
